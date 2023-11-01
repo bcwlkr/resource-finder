122d9e8a7e6bbdc2d2b858d5842e7103af38cfbc
bcwlkr <50838341+bcwlkr@users.noreply.github.com>
2023-11-01 15:27:48 +0000

    done for nate

--- Main.java
+++ Main.java
@@ -1,20 +1,27 @@
+import java.util.Arrays;
 // Resource Finder Starter Code              A+CR
 class Main {
   public static void main(String[] args) {
     Measure world = new Measure();
-		
+		double[] row1 = {};
     for(int r = 0; r < 30; r++){
       for(int c = 0; c < 20; c++){
         // Write your code here.
         double material = world.getMaterial(r, c);
+        
         material = Math.round(material * 100.0) / 100.0;
-        System.out.println(material);
-       
+        
+        if (material >= 0.34 && material <= 0.56) {
+          System.out.println(material + " unpacked snow " + r + " " + " " +c);
+        }
+        else if (material >= 1.9 && material <= 2.4){
+          System.out.println(material + " sed rock " + r + " " + " " +c);
+        }
         // This marks the end of your code segment.
       }
-    } 
-     /* this code allows the program to finish displaying all the values to the console
-     you must press the STOP button to stop the program from running */
-     while (true);    
+    }
+    /* this code allows the program to finish displaying all the values to the console
+    you must press the STOP button to stop the program from running */
+    while (true);
   }
 }
---
+++ storage.java
@@ -0,0 +1,15 @@
+if (material >= 0.35 && material <= 0.55) {
+          System.out.println(material + " unpacked snow");
+        }
+        else if (material >= 0.83 && material <= 0.97){
+          System.out.println(material + " glacial ice");
+        }
+        else if (material >= 2.21 && material <= 2.76){
+          System.out.println(material + " sandstone");
+        }
+        else if (material >= 1.77 && material <= 3.2){
+          System.out.println(material + " shale");
+        }
+        else if (material >= 1.93 && material <= 2.4){
+          System.out.println(material + " limestone");
+        }
